ed629a890fdbb5fcc0a5fcad80672eb2d80de81f
avict <avictorg@gmail.com>
2022-03-09 01:37:59 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./static/styles.css";
-import "./static/bootstrap/css/bootstrap.min.css";
+import "./static/bootstrap/css/bootstrap.css";
 import "./static/bootstrap-icons/bootstrap-icons.css";
-import "./static/boxicons/css/boxicons.min.css";
+import "./static/boxicons/css/boxicons.css";
 import "./static/boxicons/css/animations.css";
 import About from "./section/about";
 import Contact from "./section/contact";
@@ -17,7 +17,7 @@ function App() {
         <nav id="navbar" class="navbar nav-menu">
           <ul>
             <li>
-              <a href="#hero" class="nav-link scrollto active">
+              <a href="#hero" class="nav-link scrollto">
                 <i class="bx bx-home"></i>
                 <span>Home</span>
               </a>
@@ -30,22 +30,26 @@ function App() {
             </li>
             <li>
               <a href="#resume" class="nav-link scrollto">
-                <i class="bx bx-file-blank"></i> <span>Resumo</span>
+                <i class="bx bx-file-blank"></i>
+                <span>Resumo</span>
               </a>
             </li>
             <li>
               <a href="#services" class="nav-link scrollto">
-                <i class="bx bx-server"></i> <span>Linguagens</span>
+                <i class="bx bx-server"></i>
+                <span>Linguagens</span>
               </a>
             </li>
             <li>
               <a href="#projetos" class="nav-link scrollto">
-                <i class="bi bi-code-slash"></i> <span>Projetos</span>
+                <i class="bi bi-code-slash"></i>
+                <span>Projetos</span>
               </a>
             </li>
             <li>
               <a href="#contact" class="nav-link scrollto">
-                <i class="bx bx-envelope"></i> <span>Contato</span>
+                <i class="bx bx-envelope"></i>
+                <span>Contato</span>
               </a>
             </li>
           </ul>
